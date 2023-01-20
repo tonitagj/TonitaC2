52db66ba61c7deb8336e8b4ea939dd40822d33cb
Tonita <tonitagjeluci@gmail.com>
2023-01-20 01:11:15 +0100

    This is the main commit for now.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,32 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+
+public class Main {
+    private static final String POSTS_API_URL = "http://localhost:8080/posts";
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .header("accept", "application/json")
+                .uri(URI.create(POSTS_API_URL))
+                .build();
+      HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        //System.out.println(response.body());
+
+        //Parse Jsons into Objects
+        ObjectMapper mapper = new ObjectMapper();
+
+        List<Post> posts = mapper.readValue(response.body(), new TypeReference<List<Post>>(){} );
+        posts.forEach(System.out::println);
     }
 }
